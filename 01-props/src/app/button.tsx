69c21ffb6ff71061c1ef06f090c42f41ea0018f5
zaibali <zaibali51@live.com>
2024-10-15 11:27:06 +0500

    props from parent to child and grandchild

---
+++ 01-props/src/app/button.tsx
@@ -0,0 +1,14 @@
+"use client"
+type MyButtonTypes = {
+  label: string,
+  clickHandler: () => void;
+}
+export default function MyButton({label, clickHandler} : MyButtonTypes) {
+  // const eventHandler = () =>{
+  //   console.log("this is event handler")
+  // }
+ 
+  // return <button onClick={eventHandler}>{label}</button>
+
+  return <button onClick={clickHandler}>{label}</button>
+}
---
+++ 01-props/src/app/helloworld.tsx
@@ -0,0 +1,26 @@
+import MyButton from "./button";
+
+type HelloWorldTypes = {
+  greet: string,
+  message: string,
+  btnLabel: string,
+  handerFunc: ()=> void;
+}
+
+export default function HelloWorld({
+  greet,
+  message,
+  btnLabel,
+  handerFunc
+} : HelloWorldTypes)  {
+  return (
+    <>
+     <h1>{greet}</h1>
+     <p>{message}</p>
+     <MyButton
+     label={btnLabel}
+     clickHandler={handerFunc}
+     />
+    </>
+  )
+}
