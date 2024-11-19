d9e50adbeedb83824dc3c14ea9fee3234f959570
zaibali <zaibali51@live.com>
2024-11-19 11:26:13 +0500

    login component added with react hook form and shadcn also added in project

--- 03-auth-example/src/app/login.tsx
+++ 03-auth-example/src/app/login.tsx
@@ -1,7 +1,90 @@
-import React from 'react'
+import React from 'react';
 
-export default function login() {
+// React Hook Form to imports
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email().min(2).max(50),
+  password: z.string()
+    .min(8) // Minimum length
+    .max(100) // Maximum length
+    .regex(/[a-z]/, 'Must contain at least one lowercase letter') // Lowercase letter
+    .regex(/[A-Z]/, 'Must contain at least one uppercase letter') // Uppercase letter
+    .regex(/[0-9]/, 'Must contain at least one number') // Number
+    .regex(/[\W_]/, 'Must contain at least one special character'), // Special character
+});
+
+export default function Login() {
+  // 1. Define your form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    console.log(values);
+  }
+
   return (
-    <div>login</div>
-  )
+    <>
+      <h2>Please Login</h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Email"
+                    {...field}
+                    type="text"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Password"
+                    {...field}
+                    type="password"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
 }
--- 03-auth-example/src/app/page.tsx
+++ 03-auth-example/src/app/page.tsx
@@ -2,19 +2,21 @@
 
 import { useState } from "react";
 import UserTimeline from "./usertimeline";
+import Login from "./login";
+
+
 
 export default function Home() {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <>
       {
         isAuthenticated ? (
           <UserTimeline />
         ) : (
-          <h2>Not Auth run</h2>
+          <Login />
         )
       }
-
 
     </>
   );
