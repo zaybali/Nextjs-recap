b106da8f8e289b6373fd0bcf8d529a6633f470cc
zaibali <zaibali51@live.com>
2024-11-20 14:28:56 +0500

    myInfo component added

---
+++ 04-product-app-2/src/app/myinfo.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function MyInfo() {
+  return (
+    <>
+      <div>myinfo</div>
+      <table style={{ border: '1px solid black' }}>
+        <thead >
+          <td style={{ border: '1px solid black', padding: '10px 40px' }}>01</td>
+          <td style={{ border: '1px solid black', padding: '10px 40px' }}>Zaib Ali</td>
+          <td style={{ border: '1px solid black', padding: '10px 40px' }}>Karachi</td>
+        </thead>
+      </table>
+    </>
+  )
+}
--- 04-product-app-2/src/app/page.tsx
+++ 04-product-app-2/src/app/page.tsx
@@ -1,9 +1,11 @@
+import MyInfo from "./myinfo";
 
 export default function Home() {
   return (
     <>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", height: '90vh' }}>
         <h1>Product Filter App</h1>
+        <MyInfo />
       </div>
     </>
   );
