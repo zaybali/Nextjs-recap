983574f1af4f26bdd6b76b726a6eeb526def9db2
zaibali <zaibali51@live.com>
2024-11-20 13:10:40 +0500

    initialize product filter app 2

---
+++ 04-product-app-2/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", height: '90vh' }}>
+        <h1>Product Filter App</h1>
+      </div>
+    </>
+  );
+}
