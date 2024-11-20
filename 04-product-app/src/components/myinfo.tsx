eaed4aefdf42f094150017886d87ad7ab0ffd752
zaibali <zaibali51@live.com>
2024-11-20 12:25:23 +0500

    indentation fix

--- 04-product-app/src/components/myinfo.tsx
+++ 04-product-app/src/components/myinfo.tsx
@@ -3,15 +3,15 @@ import { Flex, Table } from "@chakra-ui/react"
 export default function MyInfo() {
   return (
     <>
-        <Table.Root size="lg" width="50%">
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeader>01</Table.ColumnHeader>
-              <Table.ColumnHeader>Zaib Ali</Table.ColumnHeader>
-              <Table.ColumnHeader>Karachi</Table.ColumnHeader>
-            </Table.Row>
-          </Table.Header>
-        </Table.Root>
+      <Table.Root size="lg" width="50%">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>01</Table.ColumnHeader>
+            <Table.ColumnHeader>Zaib Ali</Table.ColumnHeader>
+            <Table.ColumnHeader>Karachi</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+      </Table.Root>
     </>
   )
 }
